818379b5d5445a860e022ab411fefa45bd0f8d41
liisaera <liisa.eranen@metropolia.fi>
2021-02-09 12:58:58 +0200

    Validointi
    Korjaukset validoinnin jälkeen

--- footer.php
+++ footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fi">
-<head>
-        <meta charset="utf-8">
-        <title>Kotitehtava 10</title> 
-</head>
-<body>
-
 <?php
     echo "<p>Copyright &copy;" . date("Y") . " Liisa Eränen";
 ?>
-
-</body>
--- header.php
+++ header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fi">
-<head>
-        <meta charset="utf-8">
-        <title>Kotitehtava 10</title> 
-</head>
-<body>
-
 <?php
     echo "Tämä on header."
 ?>
-
-</body>
